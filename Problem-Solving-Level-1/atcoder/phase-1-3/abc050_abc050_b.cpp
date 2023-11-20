1811257bebe7b281557a9a0bc7bb206faf8153c2
Mayar <maimagdy238@gmail.com>
2023-11-20 03:02:02 +0200

    update level 1

--- Problem-Solving-Level-1/atcoder/phase-1-3/abc050_abc050_b.cpp
+++ Problem-Solving-Level-1/atcoder/phase-1-3/abc050_abc050_b.cpp
@@ -1,3 +1,33 @@
-//
-// Created by mai on 11/20/2023.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+#define _Mayar_                      \
+                 ios_base::sync_with_stdio(0); \
+                 cin.tie(0);                   \
+                 cout.tie(0);
+typedef long long ll;
+#define sz(x) int(x.size())
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
+#endif
+    _Mayar_
+    int tc = 1;
+//    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n;
+        int a[n], sum = 0;
+        for (int i = 0; i < n; ++i) cin >> a[i], sum += a[i];
+        cin >> m;
+        while (m--) {
+            int ind, t;
+            cin >> ind >> t;
+            cout << (sum - a[ind - 1]) + t << "\n";
+
+        }
+    }
+
+}
